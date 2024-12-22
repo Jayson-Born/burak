cb7553f68fa0a40dbb992d43b9dba79eaf3dcd83
Jayson-Born <abdukabir34@gmail.com>
2024-12-23 03:39:22 +0900

    feat:express integrations

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,17 @@
 import dotenv from "dotenv";
 dotenv.config();
-
 import mongoose from 'mongoose';
  mongoose.set('strictQuery',true)
+ import app from "./app";
+
 mongoose
 .connect(process.env.Mongo_URL as string, {})
 .then ((data)=> {
     console.log("MongoDB connected successfully")
     const PORT=process.env.PORT ?? 3003;
+    app.listen(PORT, function(){
+        console.log(`The server is successfully runing on port ${PORT}`)
+    }); 
 }) 
 .catch((err)=>{ console.log("there is a problem with connection MongoDB")});
 
