61d61851e11b1485a1e347cdbea14b3c6c9043a9
Jayson-Born <abdukabir34@gmail.com>
2024-12-24 15:23:11 +0900

    feat: develop member schema model

--- src/router.ts
+++ src/router.ts
@@ -2,6 +2,6 @@ import express,{Request, Response} from 'express';
 const router = express.Router();
 import memberController from "./controller/member.controller";
 
-
+//React uchun
 
 export default router;
