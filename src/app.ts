b1342442292f421b00920b5c2535e95c1c791a50
Jayson-Born <abdukabir34@gmail.com>
2025-02-13 21:10:52 +0900

    feat:CORS integration

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
+import cors from "cors";
 import express from 'express'
 import path from "path";
 import router from "./router";
@@ -18,11 +19,12 @@ const store = new MongoDBStore({
 
 /** 1-ENTERANCE **/
 const app= express();
-app.use(express.static(path.join(__dirname, "public")));
+app.use("/uploads", express.static("./uploads"));
 app.use("/uploads", express.static("./uploads"))
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(cookieParser())
+app.use(cors({ credentials: true, origin: true }));
 app.use(morgan(MORGAN_FORMAT));
 
 
